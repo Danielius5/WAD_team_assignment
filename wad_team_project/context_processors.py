0c3031601f9be0d4b01f7fb417153ee1d545ba02
Danielius Sukys <sukys.danielius@gmail.com>
2021-04-07 12:39:00 +0100

    fix of menu

--- wad_team_project/context_processors.py
+++ wad_team_project/context_processors.py
@@ -3,4 +3,5 @@ def add_top_books(request):
     books = list(Book.objects.order_by('average_rating').filter().all().reverse()[:5])
     return {
         'top_5_books' : books,
+        'request_user' : request.user
     }
